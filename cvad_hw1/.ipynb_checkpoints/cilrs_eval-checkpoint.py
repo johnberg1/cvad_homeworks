1f3af3b4b323ae8e171d638095e56330a8119638
Canberk Baykal <38884482+johnberg1@users.noreply.github.com>
2022-04-16 13:31:52 +0300

    first commit

---
+++ cvad_hw1/.ipynb_checkpoints/cilrs_eval-checkpoint.py
@@ -0,0 +1,69 @@
+import os
+from torchvision import transforms
+import yaml
+import torch
+
+from carla_env.env import Env
+
+
+class Evaluator():
+    def __init__(self, env, config):
+        self.env = env
+        self.config = config
+        self.agent = self.load_agent()
+
+    def load_agent():
+        # Your code here
+        model = torch.load("cilrs_model.ckpt")
+        model.cuda().eval()
+        return model
+
+    def generate_action(rgb, command, speed):
+        # Your code here
+        rgb = transforms.ToTensor()(rgb).unsqueeze(0).cuda()
+        command = torch.tensor(command).unsqueeze(0).cuda()
+        speed = torch.tensor(speed).unsqueeze(0).cuda()
+        with torch.no_grad():
+            actions, _ = self.agent(rgb, command, speed)
+        return actions.cpu().detach().numpy()
+
+    def take_step(self, state):
+        rgb = state["rgb"]
+        command = state["command"]
+        speed = state["speed"]
+        throttle, steer, brake = self.generate_action(rgb, command, speed)
+        action = {
+            "throttle": throttle,
+            "brake": brake,
+            "steer": steer
+        }
+        state, reward_dict, is_terminal = self.env.step(action)
+        return state, is_terminal
+
+    def evaluate(self, num_trials=100):
+        terminal_histogram = {}
+        for i in range(num_trials):
+            state, _, is_terminal = self.env.reset()
+            for i in range(5000):
+                if is_terminal:
+                    break
+                state, is_terminal = self.take_step(state)
+            if not is_terminal:
+                is_terminal = ["timeout"]
+            terminal_histogram[is_terminal[0]] = (terminal_histogram.get(is_terminal[0], 0)+1)
+        print("Evaluation over. Listing termination causes:")
+        for key, val in terminal_histogram.items():
+            print(f"{key}: {val}/100")
+
+
+def main():
+    with open(os.path.join("configs", "cilrs.yaml"), "r") as f:
+        config = yaml.full_load(f)
+
+    with Env(config) as env:
+        evaluator = Evaluator(env, config)
+        evaluator.evaluate()
+
+
+if __name__ == "__main__":
+    main()
---
+++ cvad_hw1/models/cilrs.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+from torchvision.models import resnet18
+import torch
+
+
+class CILRS(nn.Module):
+    """An imitation learning agent with a resnet backbone."""
+    def __init__(self):
+        # resnet18 perception module
+        super().__init__()
+        self.perception_module = resnet18(pretrained=True)
+        num_features = self.perception_module.fc.in_features
+        self.perception_module.fc = nn.Linear(num_features, 256)
+        
+        # speed input
+        self.speed_in = nn.Sequential(nn.Linear(1,128),nn.ReLU(),nn.Linear(128,128))
+        
+        # speed prediction
+        self.speed_prediction = nn.Sequential(nn.Linear(256,256),nn.ReLU(),nn.Linear(256,1))
+        
+        # conditional_module, we have 4 branches for 4 different commands
+        branches = [nn.Sequential(nn.Linear(384,256),nn.ReLU(),nn.Linear(256,256),nn.ReLU(),nn.Linear(256,3)) for i in range(4)]
+        self.branches = nn.ModuleList(branches)
+        
+
+    def forward(self, img, command, speed):
+        # extract perceptual features from resnet
+        img_features = self.perception_module(img)
+        
+        # predict the speed
+        predicted_speed = self.speed_prediction(img_features)
+        
+        # pass the speed input through the mlp
+        speed_features = self.speed_in(speed)
+        
+        # concat the perceptual and speed features
+        cond_input = torch.hstack((img_features,speed_features))
+        # select the branch based on the command, output (steer, throttle, brake)
+        predicted_actions = []
+        bs = img.shape[0]
+        for i in range(bs):
+            cmd = command[i,:]
+            predicted_action = self.branches[cmd](cond_input[i,:])
+            predicted_actions.append(predicted_action)
+        predicted_actions = torch.vstack(predicted_actions)
+        return predicted_actions, predicted_speed
+    
+        
